180801c96373b4b4d6f1711b2f0d1ade103d57a9
songpeilann <songpeilan@hefantv.com>
2018-03-29 10:00:06 +0800

    update record

--- pages/record/index.js
+++ pages/record/index.js
@@ -1,17 +1,17 @@
-var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
     toView: 'red',
-    scrollTop: 100
+    scrollTop: 100,
+    recordList: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   },
   upper: function (e) {
-    console.log(e)
+    console.log('--->upper',e)
   },
   lower: function (e) {
-    console.log(e)
+    console.log('--->lower',e)
   },
   scroll: function (e) {
-    console.log(e)
+   // console.log('scroll',e)
   },
   tap: function (e) {
     for (var i = 0; i < order.length; ++i) {
